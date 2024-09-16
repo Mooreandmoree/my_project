7291cf2a5dd3cb8929bd844a8063fcf32f5dfa94
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-09-16 20:40:29 +0100

    modified:   MyCar.rb

--- MyCar.rb
+++ MyCar.rb
@@ -1,4 +1,6 @@
 class MyCar
+  attr_accessor :color
+  attr_reader :year
 
   def initialize(y, c, m)
     @current_speed = 0
@@ -6,35 +8,43 @@ class MyCar
     @color = c
     @model = m
   end
-  
+
   def speed_up(number)
     @current_speed += number
     puts "You push the gas and accelerate #{number} mph."
   end
-    
+
   def brake(number)
     @current_speed -= number
     puts "You push the brake and decelerate to #{number} mph."
   end
-  
+
   def current_speed
-      puts "You are now going #{@current_speed} mph."
+    puts "You are now going #{@current_speed} mph."
   end
-    
+
   def shut_down(number)
     @current_speed = 0
-    puts "You switch of the ignition with the key and the car is now on the ground."
+    puts 'You switch of the ignition with the key and the car is now on the ground.'
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "You spray painted your car #{self.color}."
   end
 end
 
-    lumina = MyCar.new(1997, 'chevy lumina', 'white')
-    lumina.speed_up(20)
-    lumina.current_speed
-    lumina.speed_up(20)
-    lumina.current_speed
-    lumina.brake(20)
-    lumina.current_speed
-    lumina.brake(20)
-    lumina.current_speed
-    lumina.shut_down
-    lumina.current_speed
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.spray_paint('red')  
+puts lumina.color
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+lumina.current_speed
+
