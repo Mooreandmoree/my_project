75d88cdcd1df905462fffd537838eda1f738dd76
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-09-20 14:52:41 +0100

    modified:   MyCar.rb

--- MyCar.rb
+++ MyCar.rb
@@ -1,12 +1,28 @@
-class MyCar
-  attr_accessor :color
-  attr_reader :year
+module Hugeamount
+  def huge?(horsepower)
+    horsepower > 400
+  end
+end
 
-  def initialize(y, c, m)
+class Vehicle
+  attr_accessor :color
+  attr_reader :model, :year
+  @@number_of_vehicles = 0
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
     @current_speed = 0
-    @year = y
-    @color = c
-    @model = m
+    @@number_of_vehicles += 1
   end
 
   def speed_up(number)
@@ -16,27 +32,44 @@ class MyCar
 
   def brake(number)
     @current_speed -= number
-    puts "You push the brake and decelerate to #{number} mph."
+    puts "You push the brake and decelerate #{number} mph."
   end
 
   def current_speed
     puts "You are now going #{@current_speed} mph."
   end
 
-  def shut_down(number)
+  def shut_down
     @current_speed = 0
-    puts 'You switch of the ignition with the key and the car is now on the ground.'
+    puts "Put this car at rest my friend!"
   end
 
   def spray_paint(color)
     self.color = color
-    puts "You spray painted your car #{self.color}."
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+class MyTruck < Vehicle
+  include Hugeamount
+
+   HORSEPOWER = 400
+
+  def to_s
+    "My truck  is a #{self.color}, #{self.year}, #{self.model}!"
+  end
+end
+
+class MyCar < Vehicle
+  
+  HORSEPOWER = 150
+
+  def to_s
+    "My car is a #{self.color}, #{self.year}, #{self.model}!"
   end
 end
 
 lumina = MyCar.new(1997, 'chevy lumina', 'white')
-lumina.spray_paint('red')  
-puts lumina.color
 lumina.speed_up(20)
 lumina.current_speed
 lumina.speed_up(20)
@@ -46,5 +79,9 @@ lumina.current_speed
 lumina.brake(20)
 lumina.current_speed
 lumina.shut_down
-lumina.current_speed
-
+MyCar.gas_mileage(13, 351)
+lumina.spray_paint("red")
+puts lumina
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
